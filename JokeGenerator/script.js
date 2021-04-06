7da8b07aba002385fa1d58e7aaa6f779fcd8e482
Leo <a840331a840331@gmail.com>
2021-04-06 15:47:15 +0930

    Add files via upload

---
+++ JokeGenerator/script.js
@@ -0,0 +1,22 @@
+const joke = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+randomJoke()
+jokeBtn.addEventListener('click', randomJoke)
+//https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+function randomJoke() {
+    const config = {
+    headers: {
+           Accept: 'application/json',
+        }
+    }
+    fetch('https://icanhazdadjoke.com', config)
+        .then(response => response.json())
+        .then(data => {
+            joke.innerHTML = data.joke
+    })
+    /* async/await
+    const response = await fetch('https://icanhazdadjoke.com', config)
+    const data = await response.json()
+    joke.innerHTML = data.joke
+*/
+}
