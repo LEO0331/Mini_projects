83951709cc1f5d9031a10d6da9d21a28305c7eb2
Leo <a840331a840331@gmail.com>
2021-06-23 22:00:34 +0930

    Add files via upload

---
+++ todo/script.js
@@ -0,0 +1,34 @@
+const list = document.querySelector(".list")
+const txt = document.querySelector(".txt")
+const btn = document.querySelector(".btn")
+let data = [
+	//{"content": "todo1"}
+]
+function renderData(){
+	let str = ''
+	data.forEach((item, index) => {
+	str += `<li>${item.content} <input type="button" value="delete" class="delete" data-num="${index}"></li>`
+	})
+	list.innerHTML = str
+}
+renderData() //initialization
+btn.addEventListener("click", e => { //add
+	if(txt.value == ''){
+		alert("plz input items")
+		return
+	}
+	let obj = {}
+	obj.content = txt.value
+	data.push(obj)
+	renderData()
+	txt.value = ''
+})
+list.addEventListener("click", e => { //delete
+	if(e.target.getAttribute("class") != "delete"){
+		alert("plz click the button")
+		return
+	}
+	let num = e.target.getAttribute("data-num")
+	data.splice(num,1)
+	renderData()
+})
